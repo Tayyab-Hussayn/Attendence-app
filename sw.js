6e2781bf6abf14e0f6cb9f6c53e9488a008881a3
Tayyab Hussayn <tayyabhussayn@gmail.com>
2025-12-23 18:49:30 +0500

    Attendance app for andriod to track childern habits

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Service Worker - Makes app work offline
+const CACHE_NAME = 'ibadah-tracker-v1';
+const urlsToCache = [
+    './',
+    './Attendance.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+// Install event - cache files
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event - serve from cache
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+    );
+});
